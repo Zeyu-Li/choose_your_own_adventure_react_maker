f30d88b8404462bd4a90c9d3f51c1e11b85a641d
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-06-20 15:47:00 -0600

    working frontend for event generator

--- event_generator/js/main.js
+++ event_generator/js/main.js
@@ -1,3 +1,93 @@
+let counter = 1
+let card
+
+const row_start = `
+<div class="container main_content">
+    <div class="row justify-content-start main">
+`
+
+const row_end = `
+    </div>
+</div>
+`
+
 $( document ).ready(() => {
-    $("#root").html("<h1>Hello</h1>")
+    card = get_card()
+    $("#root").append(row_start + card + row_end)
+})
+
+function get_card() {
+    let text =  `
+<!-- each card -->
+<div class="col-4">
+    <div class="card" style="width: 29vw;"><form>
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-5 col-form-label">Key:</label>
+            <div class="col-sm-5">
+                <input type="text" class="form-control" id="inputEmail3" value="`+ counter +`">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group mb-3 spacer">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" for="inputGroupSelect01" >Text align</label>
+                </div>
+                <select class="custom-select" id="inputGroupSelect01">
+                    <option selected>Bottom</option>
+                    <option value="1">Center</option>
+                    <option value="2">Top</option>
+                </select>
+            </div>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="2" placeholder="text"></textarea>
+        </div>
+
+        <!-- background -->
+        <div class="form-row">
+            <label class="col-form-label">Background: </label>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="file name">
+            </div>
+            <div class="col">
+                <input type="color" class="form-control" placeholder="color">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Options: </label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">Press Space to Continue [`+ (counter+1) +`]</textarea>
+        </div>
+
+        <!-- fade in/out -->
+        <div class="spacer">
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                <label class="form-check-label" for="inlineCheckbox1">Fade In</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                <label class="form-check-label" for="inlineCheckbox2">Fade Out</label>
+            </div>
+        </div>
+    </form>
+    </div>
+</div>
+    `
+    counter++
+    return text
+}
+
+
+// button clicks
+$('#first button').click(()=>{
+    card = get_card()
+    $('.main_content .main').last().append(card)
+})
+
+$('#second button').click(()=>{
+    counter--
+    $('#root .col-4').last().remove()
+})
+
+$('#third button').click(() => {
+    $('.popup-container').toggle()
 })
