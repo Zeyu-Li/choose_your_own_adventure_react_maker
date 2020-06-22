af25f8ecba82a3b42ccd526f307dff69bab57294
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-06-21 21:47:19 -0600

    added build

---
+++ adventure_template/build/precache-manifest.677a14aab5eb87cd0776fa1fa9071d51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad8572e438cfdcdea48beb88e9c0d56",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5f34b2e247e4443db05",
+    "url": "./static/css/main.574fc82a.chunk.css"
+  },
+  {
+    "revision": "06bf6628121bcf4e5c0c",
+    "url": "./static/js/2.16a16486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.16a16486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f34b2e247e4443db05",
+    "url": "./static/js/main.e6e24ad2.chunk.js"
+  },
+  {
+    "revision": "f6d81d4beeffce227069",
+    "url": "./static/js/runtime-main.deea157b.js"
+  }
+]);
